d71d1efdafca6464da1b258f566b1d2ca8071a9d
zizi_fa <a.zeinab.fazeli@gmail.com>
2023-05-23 16:15:09 +0430

    create a register view and url and serializer

--- core/authentication/urls.py
+++ core/authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 # from .views import RegisterView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import CreateUserView
+
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),#use simplejwt
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),#use simplejwt
+    path('register/', CreateUserView.as_view(), name="sign_up"),
 ]
--- core/authentication/views.py
+++ core/authentication/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import CreateUserSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CreateUserView(APIView):
+    def post(self,request):
+        req_data=request.data
+        serializer=CreateUserSerializer(data=req_data)
+        serializer.is_valid(raise_exception=True)
+
+        # we have to save user password so ...
+        data = serializer.validated_data #take validate data from serializer
+        user_data=User(
+            username=data['username'],
+            email=data['email']
+        )
+        user_data.set_password(data['password'])
+        user_data.save()
+        return Response(serializer.data)
+
+
+
