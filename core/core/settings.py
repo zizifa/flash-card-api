eadd40c12996dfb469148abb7a25c1143cc324b7
zizi_fa <a.zeinab.fazeli@gmail.com>
2023-05-23 15:46:51 +0430

    fix simplejwt

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'card',
     'rest_framework',
     'authentication',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
