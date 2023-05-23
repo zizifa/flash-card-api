2913528b370bf607d44b66faabcc8fdf1473c9ff
Zizi__fa <89692143+zizifa@users.noreply.github.com>
2023-05-23 23:21:33 +0430

    add user ForeignKey

--- core/card/models.py
+++ core/card/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 
+User=get_user_model()
 class FlashCard(models.Model):
-
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     question=models.TextField()
     answer=models.TextField()
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.question
