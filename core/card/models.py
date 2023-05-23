5e7af64b32555ba7e8a03db0cb088ebc6ea37b01
zizi_fa <a.zeinab.fazeli@gmail.com>
2023-05-23 13:54:14 +0430

    create flashcard model

--- core/card/models.py
+++ core/card/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class FlashCard(models.Model):
+    # author=
+    question=models.TextField()
+    answer=models.TextField()
+
+    def __str__(self):
+        return self.question
+
+# class Author(models.Model):
+#     name=models.CharField(max_length=50)
